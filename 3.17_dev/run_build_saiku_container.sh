e55c43ad1c1adf16f0e3eef4605c114b6ea8df91
c.tamietto <c.tamietto@pokerspa.it>
2019-05-10 21:51:56 +0200

    first commit

---
+++ 3.17_dev/run_build_saiku_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+if [ ! -f /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_development/pom.xml ] 
+then
+    echo "non esiste il file /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_development/pom.xml"
+    exit -1
+fi
+
+cd /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_development
+
+docker run -it --rm --name pk_build_saiku_dev -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn clean install -DskipTests
+
+
