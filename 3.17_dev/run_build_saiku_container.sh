2158dadb1e37beabc2a3cab28492b7dfbf4edcdc
c.tamietto <c.tamietto@pokerspa.it>
2019-05-10 21:55:12 +0200

    changed name of saiku source directory

--- 3.17_dev/run_build_saiku_container.sh
+++ 3.17_dev/run_build_saiku_container.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-if [ ! -f /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_development/pom.xml ] 
+if [ ! -f /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_src_3.17_dev/pom.xml ] 
 then
-    echo "non esiste il file /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_development/pom.xml"
+    echo "non esiste il file /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_src_3.17_dev/pom.xml"
     exit -1
 fi
 
-cd /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_development
+cd /home/ctamietto/git/workspace/pkbuildsaiku/development/saiku_src_3.17_dev
 
 docker run -it --rm --name pk_build_saiku_dev -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.6.1-jdk-8 mvn clean install -DskipTests
 
